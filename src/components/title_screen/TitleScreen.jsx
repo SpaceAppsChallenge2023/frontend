ec8bb83d1c65be7bd1af6269b19b7a12c9d01da7
Andrés Martínez <autummata@gmail.com>
2023-10-07 13:10:45 -0700

    finished warning scroll, started focusing on main features

--- src/components/title_screen/TitleScreen.jsx
+++ src/components/title_screen/TitleScreen.jsx
@@ -5,17 +5,24 @@ const TitleScreen = () => {
     <div className = "min-h-screen flex flex-col justify-center items-center w-full text-center">
 
         <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-          <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+          <span class="text-transparent bg-clip-text bg-gradient-to-r to-rose-600 from-yellow-500">
             Melodies
           </span>{" "}
           from Space.
         </h1>
-        <p class="mb-8 text-2xl text-gray-500 dark:text-gray-400">
+        <p class="mb-8 text-2xl font-bold text-white dark:text-white">
           Let's listen to the stars.
         </p>
-        <p class="text-sm font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+        <i class="text-sm font-normal text-gray-500 lg:text-xl dark:text-gray-400">
           (Click anywhere to start)
-        </p>
+        </i>
+
+        
+        <p className="absolute bottom-0 mb-4 text-sm font-normal text-gray-500 lg:text-sm dark:text-gray-400">
+        Made by the Sonic Revolution Team
+      </p>
+
+
 
     </div>
   );
--- src/components/warning/Warning.jsx
+++ src/components/warning/Warning.jsx
@@ -1,25 +1,103 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
-import WarningLogo from "./white_alert.svg";
-const Warning = () => {
-  return (
-    <div className="flex ml-3 min-h-screen flex-col justify-center items-center">
-      <div>
-        <img src={WarningLogo} alt="warning" className="content-evenly" />
-      </div>
-      <div className="flex-col ml-7">
-        <h1 className="text-6xl font-bold dark:text-white">Warning</h1>
+import Headphones from "../../assets/headphon.png";
+import NasaLogo from "../../assets/nasa_logo.png";
+import Ear from "../../assets/ear.png";
+import Particles from "react-tsparticles";
+import ParticleConfig from "../../config/particles.json";
+import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
 
-        <div className = "text-xl">
-          <p className="mt-3"> Please turn up the volume before using it. </p>
-          <div className="mt-4">
-            <Link to="/home" className="mt-5">
-              Start
-            </Link>
+const Warning = () => {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    //await loadFull(engine);
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
+  return (
+    <div className="text-3xl">
+      <div className="h-screen flex flex-col justify-center items-center w-full">
+        <div className="relative h-screen flex flex-col justify-center items-center w-screen ">
+          <div
+            className="absolute bottom-24 flex flex-row justify-around items-center w-screen h-64"
+            id="fernanda"
+          >
+            <div className="h-32 w-3/5 text-normal text-left">
+              <Link to="/" className="text-sm">
+                {" "}
+                Return to title screen.{" "}
+              </Link>
+              <p className="">
+                The objective of this project is to create sonifications based
+                on footage provided by NASA.
+                <br />
+                <i className="font-thin text-lg"> Scroll down </i>
+              </p>
+            </div>
+
+            <div className="h-64 w-72 flex items-center">
+              <img src={NasaLogo} alt="nasa logo" />
+            </div>
           </div>
         </div>
       </div>
+
+      <div className="h-screen flex flex-col justify-center items-center w-full text-center">
+        <p className="text-3xl">This program was made with the intent of...</p>
+      </div>
+
+      <div className="h-screen flex flex-col justify-center items-center w-full text-center bg-red-500 relative overflow-hidden">
+        <Particles
+          className="absolute inset-0 z-0"
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={ParticleConfig}
+        />
+        <p className="z-10 pb-5 relative text-5xl font-bold">
+          Learning about the universe that surrounds us.
+        </p>
+      </div>
+
+      <div className="h-screen flex flex-col justify-center items-center w-full text-center blur-sm hover:blur-none transition-all duration-300">
+        <p className="text-9xl"> 👓 </p>
+        <p className = "text-3xl font-thin">
+        With new glasses.
+      
+
+        </p>
+      </div>
+
+      <div className="h-screen flex flex-col justify-center items-center w-full text-center ">
+        <p>Or, in this case...</p>
+      </div>
+
+      <div className="h-screen flex flex-col justify-center items-center w-full text-center ">
+        <p className="text-9xl"> 👂🎧 </p>
+        <p className="text-2xl">With our ears.</p>
+      </div>
+
+      <Link to="/">
+        <div className="h-screen flex flex-col justify-center items-center w-full text-center text-xl ">
+          <div className="shadow-lg justify-middle p-32 flex flex-col items-center rounded-2xl">
+            <p className="text-9xl"> 🎧 </p>
+            <p className="text-5xl font-thin pt-16 hover:text-sky-500 transition ">
+              Best used with headphones
+            </p>
+
+            <i className="text-sm font-thin text-gray-500 lg:text-xl dark:text-gray-400 pt-16">
+              Click anywhere to start{" "}
+            </i>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 };
